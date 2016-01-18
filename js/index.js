bafca39845a3a586888831291584dec6f534df41
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-01-17 19:34:50 -0500

    stuffz

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	var scrollChk = function() {
-		if (window.pageYOffset > 350) {
+		if (window.pageYOffset > 340) {
 			$("nav").removeClass("aboveVideo");
 		} else {
 			$("nav").addClass("aboveVideo");
